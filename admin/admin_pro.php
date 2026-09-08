2db94495f9fe91ad7ea6ab74a642106b9894e0b8
Gabriel_S_Santos <riv3rfall2020@gmail.com>
2026-09-08 11:59:05 -0300

    telas de usuario e administrador

--- admin/admin_pro.php
+++ admin/admin_pro.php
@@ -1,12 +1,18 @@
 <?php
 session_start();
-require_once("./conexao.php");
+require_once("../conexao.php");
+$conexao =  mysqli_connect($servidor, $user, $senha, $banco);
 
 $usuario = $_POST["usuario"];
 $senha = $_POST["senha"];
 
-$sql = "select * from administrador where usuario = '$usuario' and senha = '$senha'";
-$sql_result = mysqli_query($conn,$sql);
+$sql = "select * from administrador where nome = '$usuario' and senha = '$senha'";
+$sql_result = mysqli_query($conexao, $sql);
 
-
-?>
+if (mysqli_num_rows($sql_result) > 0) {
+    header("location: ./admin_painel.php");
+    exit();
+} else {
+    header("location: ./admin_log_erro.html");
+    exit;
+}
---
+++ usuario/user_painel.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+include_once("../conexao.php")
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Selecione o andar</h1><hr>
+    <a href=""><button>térreo</button></a><br>
+    <a href=""><button>Andar 1</button></a><br>
+    <a href=""><button>Andar 2</button></a><br>
+    <a href=""><button>Andar 3</button></a><br>
+</body>
+</html>
